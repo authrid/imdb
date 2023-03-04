b79ae382df433086a160dd95d8fda2b240582376
authrid <power.of.author@gmail.com>
2023-03-05 01:07:07 +0700

    Test After Adding Tailwind

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Header from './components/header'
 
 export const metadata = {
   title: 'IMDB App',
@@ -8,7 +9,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header>
+
+        </Header>
+        {/* Navbar */}
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
   )
 }
