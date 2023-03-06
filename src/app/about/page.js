d86e2a84e670bd78f5e61c7bda26b1cbd56d268a
authrid <power.of.author@gmail.com>
2023-03-06 17:59:30 +0700

    Fixing bug quote

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -5,7 +5,7 @@ export default function About() {
     <div className="max-w-6xl mx-auto space-y-4 p-4">
         <h1 className="text-2xl- font-medium text-amber-600">About</h1>
         <p>
-        Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
+        Used by some of the worlds largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
         </p>
     </div>
   )
