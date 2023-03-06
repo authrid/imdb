ac621299b8c3582cccc5422586b2af4848b08e4d
authrid <power.of.author@gmail.com>
2023-03-06 17:53:50 +0700

    Add padding header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -6,7 +6,7 @@ import DarkModeSwitch from './darkmodeswitch'
 
 export default function Header() {
   return (
-    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center">
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
         <div className="flex">
             <MenuItem title="HOME" address="/" Icon={AiFillHome} />
             <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
@@ -15,7 +15,7 @@ export default function Header() {
           <DarkModeSwitch />
           <Link href="/">
             <h2 className="text-2xl">
-              <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg">IMDB</span>
+              <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDB</span>
               <span className="text-xl hidden sm:inline">Clone</span></h2>
           </Link>
         </div>
