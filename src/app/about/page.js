6038b375757de8bacfa10ef6a34b44a9cdb67e3b
authrid <power.of.author@gmail.com>
2023-03-06 12:00:09 +0700

    Adding About Page

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-x-4">
+        <h1 className="text-2xl- font-medium text-amber-600">About</h1>
+        <p>
+        Used by some of the world's largest companies, Next.js enables you to create full-stack web applications by extending the latest React features, and integrating powerful Rust-based JavaScript tooling for the fastest builds.
+        </p>
+    </div>
+  )
+}
