b62c30e1b9f230c967e34778804a1e0a91d30cfd
authrid <power.of.author@gmail.com>
2023-03-07 14:47:25 +0700

    Fetch data from tmdb API

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Results from './components/results';
+const API_KEY = process.env.API_KEY
 
-const inter = Inter({ subsets: ['latin'] })
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate: 10000}});
 
-export default function Home() {
+  if(!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
   return (
-    <h1 className="text-red-400">Home</h1>
+    <div><Results results={results}></Results></div>
   )
 }
